1c9f7efcb3ac4c3a40fcf71481d3ea5f3c31c3a0
MohammedHammouri03 <97105790+MohammedHammouri03@users.noreply.github.com>
2022-10-22 23:03:43 +0200

    Erste Implementierung für REST-API

---
+++ src/main/java/de/htwberlin/webtech/web/PersonalRestController.java
@@ -0,0 +1,28 @@
+package de.htwberlin.webtech.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class PersonalRestController {
+
+    private List<Person> persons;
+
+    public PersonalRestController() {
+        persons = new ArrayList<>();
+        persons.add(new Person(1, "Max", "Mustermann"));
+        persons.add(new Person(2, "Erika", "Mustermann"));
+    }
+
+    @GetMapping("/api/person")
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<Person> fetchPersons() {
+        return persons;
+    }
+}
