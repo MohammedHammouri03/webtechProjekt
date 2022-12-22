7dce4dd7e82837a66a01491f712e031b4f6a3809
MohammedHammouri03 <97105790+MohammedHammouri03@users.noreply.github.com>
2022-12-22 23:24:51 +0100

    test if the application works

--- src/test/java/de/htwberlin/webtech/WebtechApplicationTests.java
+++ src/test/java/de/htwberlin/webtech/WebtechApplicationTests.java
@@ -1,6 +1,8 @@
 package de.htwberlin.webtech;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,4 +12,10 @@ class WebtechApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	@DisplayName("check if the main method is working")
+	void testMain() {
+		String[] args = new String[]{"--spring.config.name=test"};
+		SpringApplication.run(WebtechApplication.class, args);
+	}
 }
